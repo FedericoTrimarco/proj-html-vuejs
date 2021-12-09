67370b365eedd0e902628d7dc21770d32a3f5ed3
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-12-09 13:15:16 +0100

    complete header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,42 +1,32 @@
 <template>
     <header class="border">
         <!-- header-top -->
-        <div class="header-top container border d-flex align-items-center justify-content-between py-2">
-            <!-- logo -->
-            <div class="logo">
-                <img src="../assets/dark-pet-logo.png" alt="logo">
+        <div class="header-top ">
+            <div class="container border d-flex align-items-center justify-content-between py-2">
+                <!-- logo -->
+                <div class="logo">
+                    <img src="../assets/dark-pet-logo.png" alt="logo">
+                </div>
+                <!-- input -->
+                <div class="input">
+                    <i class="fas fa-search me-1"></i>
+                    <input type="text" placeholder="search...">
+                </div>
+                <!-- Questions -->
+                <span><strong>Questions?</strong> Call us: 1.800.123.4567</span>
+                <!-- icons -->
+                <div class="icons">
+                    <i class="fas fa-archive me-1"></i>
+                    <i class="far fa-user"></i>
+                </div>
             </div>
-            <!-- input -->
-            <div class="input">
-                <i class="fas fa-search me-1"></i>
-                <input type="text" placeholder="search...">
-            </div>
-            <!-- Questions -->
-            <span><strong>Questions?</strong> Call us: 1.800.123.4567</span>
-            <!-- icons -->
-            <div class="icons">
-                <i class="fas fa-archive me-1"></i>
-                <i class="far fa-user"></i>
-            </div>
+
         </div>
         <nav class="header-bottom container">
             <ul class="d-flex list-none border justify-content-center">
-                <li class="me-3">
-                    <a href="/" class="text-decoration-none">fsefwfewe <i class="fas fa-angle-down ms-1"></i></a>
+                <li v-for="(link, id) in navLink" :key="id" class="me-3">
+                    <a @click.prevent href="/" class="text-decoration-none">{{ link }} <i class="fas fa-angle-down ms-1"></i></a>
                 </li>
-                <li class="me-3">
-                    <a href="/" class="text-decoration-none">fsefwfewe <i class="fas fa-angle-down ms-1"></i></a>
-                </li>
-                <li class="me-3">
-                    <a href="/" class="text-decoration-none">fsefwfewe <i class="fas fa-angle-down ms-1"></i></a>
-                </li>
-                <li class="me-3">
-                    <a href="/" class="text-decoration-none">fsefwfewe <i class="fas fa-angle-down ms-1"></i></a>
-                </li>
-                <li class="me-3">
-                    <a href="/" class="text-decoration-none">fsefwfewe <i class="fas fa-angle-down ms-1"></i></a>
-                </li>
-                
             </ul>
         </nav>
     </header>
@@ -44,31 +34,44 @@
 
 <script>
 export default {
-name: 'Header',
+    name: 'Header',
+    data() {
+        return{
+            navLink: ['Home', 'Shop', 'About', 'Blog', 'Contact', 'Shop by brand']
+        }
+    }
 }
+    
 </script>
 
 <style scoped lang="scss">
 @import '@/Style/utilities.scss';
+@import '@/Style/variables.scss';
     .header-top{
-        div,span{
-            border: 1px solid blue;
-        }
+        border-bottom: 1px solid $lightGray;
         .input{
-            background: #f3f4f3;
+            background: $lightGray;
             padding: 15px;
             border-radius: 30px;
-            width: 500px;
+            color: $naturalGrey;
             input{
                 border: none;
                 background: transparent;
+                outline: none;
+                width: 500px;
+                font-size: 20px
             }
         }
     }
     .header-bottom{
+        padding: 15px;
         li{
             a{
-                color: #9b9b9b;
+                color: $naturalGrey;
+                font-weight: bold;
+                &:hover, &:focus{
+                    color: #000;
+                }
             }
             i{
                 font-size: 10px;
