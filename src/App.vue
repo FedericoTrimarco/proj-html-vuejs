edf0b8328f1b58121c1dc099325b3e9a4229c4dd
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-12-09 14:42:47 +0100

    add SectionOne

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="app">
-    <Header />
+    <Header :links="navLink"/>
+
+    <main>
+      <SectionOne />
+    </main>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import SectionOne from '@/components/SectionOne.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    SectionOne,
+  },
+  data() {
+    return{
+      navLink: ['Home', 'Shop', 'About', 'Blog','Contact', 'Shop by brand']
+    }
   }
 }
 </script>
---
+++ src/components/SectionOne.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="section-one border">
+        <div class="section-one-top border">
+            <div class="container border d-flex justify-content-between">
+                <p><i class="far fa-map text-white"></i> <span class="text-white">International</span> International Shipping Service</p>
+                <p><i class="far fa-clock text-white"></i> See our <span class="text-white">store hours</span> and service</p>
+                <p><i class="fas fa-check text-white"></i> We price match to give you the <span class="text-white">best deals</span></p>
+            </div>
+        </div>
+        <div class="section-one-center border">
+            tlhthlprhl
+        </div>
+        <div class="section-one-bottom border">
+            tkhtr,htrk
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SectionOne',
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/Style/utilities.scss';
+@import '@/Style/variables.scss';
+
+.section-one-top{
+    background-image: url('../assets/bg-transparent-3.png');
+    background-color: $heavyMetal;
+    .container{
+        padding: 20px 100px;
+        p{
+            color: $westar; 
+        }
+    }
+}
+
+</style>
