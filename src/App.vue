b87910b980c06b1b66734afd9c7b5a5c0f366a3f
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-12-10 15:05:44 +0100

    add SectionSix

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
         title="Browse by category"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores laudantium, reprehenderit nam ad ipsam iste perferendis aut dolorum adipisci delectus veniam nulla."
       />
-      <SectionThree :mainArray="images"/>
+      <SectionThree :mainArray="articles1"/>
       <SectionFour :arrayElements="foods"/>
       <SectionFive />
-      <SectionSix />
+      <SectionSix :mainArray="articles2"/>
     </main>
   </div>
 </template>
@@ -40,7 +40,7 @@ export default {
     return{
       navLink: ['Home', 'Shop', 'About', 'Blog','Contact', 'Shop by brand'],
 
-      images: [
+      articles1: [
         {
           src: '/product-8.jpg',
           title: 'Bed(1)',
@@ -56,6 +56,30 @@ export default {
         {
           src: '/product-4.jpg',
           title: 'Transport(6)',
+        }
+      ],
+
+      articles2: [
+        {
+          src: '/product-21.jpg',
+          title: 'Transport cage',
+          price: '$25.00',
+        },
+        {
+          src: '/product-20.jpg',
+          title: 'Dog leash',
+          price: '$25.00',
+        },
+        {
+          src: '/product-16.jpg',
+          title: 'Animal transport cage',
+          price: '$25.00',
+          originalPrice: '$35.00',
+        },
+        {
+          src: '/product-11.jpg',
+          title: 'Colorful cat leash',
+          price: '$12.00',
         }
       ],
 
--- src/components/SectionSix.vue
+++ src/components/SectionSix.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="section-six container border">
-      <!-- info -->
-        <div class="section-six-top border d-flex justify-content-between align-items-end">
+    <div class="section-six container border">
+        <!-- info -->
+        <div class="section-six-top border d-flex justify-content-between align-items-end mb-5">
             <div class="section-six-top-info">
                 <span class="fw-bold ">All time best sellers</span>
                 <p>Items everyone loves</p>
@@ -10,13 +10,31 @@
         </div>
         <!-- products -->
         <div class="section-six-bottom border">
+            <!-- <CardProduct /> -->
+            <ul class="list-none d-flex border justify-content-between text-center">
+                <li v-for="(el, id) in mainArray" :key="`product-${id}`">
+                    <CardProduct
+                        :src="el.src"
+                        :title="el.title"
+                        :originalPrice="el.originalPrice"
+                        :price="el.price"
+                    />
+                </li>
+            </ul>
         </div>
-  </div>
+    </div>
 </template>
 
 <script>
+import CardProduct from '@/components/CardProduct.vue'
 export default {
     name:'SectionSix',
+    props:{
+        mainArray: Array,
+    },
+    components:{
+        CardProduct,
+    }
     
 }
 </script>
@@ -36,6 +54,12 @@ export default {
             font-family: 'Domine', serif;
             font-size: 40px;
             margin-top: 30px
+        }
+    }
+    .section-six-bottom{
+        li{
+            width: calc(100% / 4);
+            padding: 0 20px;
         }
     }
 }
