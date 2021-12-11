d3368ceb3e0254474d5a50d2254a2646bc582025
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-12-11 16:45:38 +0100

    create allArrays

---
+++ src/Data/allArrays.js
@@ -0,0 +1,161 @@
+      
+    export const navLink = ['Home', 'Shop', 'About', 'Blog','Contact', 'Shop by brand']
+
+    export const articles1 = [
+    {
+        src: '/product-9.jpg',
+        title: 'Bed(1)',
+    },
+    {
+        src: '/product-3.jpg',
+        title: 'Food(6)',
+    },
+    {
+        src: '/product-2.jpg',
+        title: 'Toys(6)',
+    },
+    {
+        src: '/product-4.jpg',
+        title: 'Transport(6)',
+    }
+    ]
+
+    export const articles2 = [
+    {
+        src: '/product-21.jpg',
+        title: 'Transport cage',
+        price: '$25.00',
+    },
+    {
+        src: '/product-20.jpg',
+        title: 'Dog leash',
+        price: '$25.00',
+    },
+    {
+        src: '/product-16.jpg',
+        title: 'Animal transport cage',
+        price: '$25.00',
+        originalPrice: '$35.00',
+    },
+    {
+        src: '/product-11.jpg',
+        title: 'Colorful cat leash',
+        price: '$12.00',
+    }
+    ]
+
+    export const articles3 = [
+    {
+        src: '/product-8.jpg',
+        title: 'Colored pet bed',
+        price: '$18.00',
+        secondPrice: '$26.00'
+    },
+    {
+        src: '/product-2.jpg',
+        title: 'Colorful ball set',
+        price: '$29.00',
+    },
+    {
+        src: '/product-3.jpg',
+        title: 'Dog bone',
+        originalPrice: '$29.00',
+        price: '$18.00',
+    },
+    {
+        src: '/product-4.jpg',
+        title: 'Animal transport bag',
+        price: '$29.00',
+    },
+    {
+        src: '/product-5.jpg',
+        title: 'Animal transport cage',
+        price: '$35.00',
+    },
+    {
+        src: '/product-10.jpg',
+        title: 'Closable cat litter',
+        price: '$16.00',
+    }
+    ]
+
+    export const blogs = [
+    {
+        src: '/blog-1.jpg',
+        title: "How to 'loose leash walk' your dog",
+        date: 'April 21st, 2020',
+    },
+    {
+        src: '/blog-2.jpg',
+        title: 'Stop cats from scratching furniture',
+        date: 'April 19st, 2020',
+    },
+    {
+        src: '/blog-3.jpg',
+        title: 'Where to buy the best pet food',
+        date: 'April 18st, 2020',
+    },
+    {
+        src: '/blog-4.jpg',
+        title: 'Proper care for your pet toys',
+        date: 'April 17st, 2020',
+    }
+    ]
+
+    export const foods = [
+    {
+        srcFood: '/food-transparent-18.png',
+        title: 'Kibble',
+        description: 'Dry dog food',
+        btnText: 'Shop dry food'
+    },
+    {
+        srcFood: '/food-transparent-17.png',
+        title: 'Moist',
+        description: 'Canned do food',
+        btnText: 'Shop moist food'
+    },
+    {
+        srcFood: '/food-transparent-16.png',
+        title: 'Frozen',
+        description: 'Freeze-Dried do food',
+        btnText: 'Shop frozen food'
+    }
+    ]
+
+    export const users = [
+    {
+        userSrc: '/avatar-3.jpg',
+        info: 'Lisa Smith consectetur adipisicing elit. Labore maxime modi porro totam reiciendis dolore aliquam itaque quos inventore illum?',
+        name: 'Lisa Smith, ThemeFusion'
+    },
+    {
+        userSrc: '/avatar-1.jpg',
+        info: 'Melissa Green consectetur adipisicing elit. Labore maxime modi porro totam reiciendis dolore aliquam itaque quos inventore illum?',
+        name: 'Melissa Green, ThemeFusion'
+    },
+    {
+        userSrc: '/avatar-4.jpg',
+        info: 'Sam Lewis consectetur adipisicing elit. Labore maxime modi porro totam reiciendis dolore aliquam itaque quos inventore illum?',
+        name: 'Sam Lewis, ThemeFusion'
+    },
+    ]
+
+    export const infoIcons = [
+    {
+        iconClass: 'fas fa-truck',
+        iconText: 'Free worldwide deliveries'
+    },
+    {
+        iconClass: 'far fa-map',
+        iconText: 'Find stores near you'
+    },
+    {
+        iconClass: 'fas fa-dollar-sign',
+        iconText: 'Best prices guarranteed'
+    },
+    {
+        iconClass: 'far fa-credit-card',
+        iconText: 'All credit card accepted'
+    },
+    ]
